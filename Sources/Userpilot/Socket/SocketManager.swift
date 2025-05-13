503a136a4975f17e6ccca370204419b4260d4d85
motasem-userpilot <motasem@userpilot.co>
2025-05-13 09:23:44 +0300

    🔧 Reset configuration

--- Sources/Userpilot/Socket/SocketManager.swift
+++ Sources/Userpilot/Socket/SocketManager.swift
@@ -208,7 +208,7 @@ extension SocketManager {
             socketState = .connecting
 
             let socketProperties: [String: Any] = [
-                SocketManager.tokenKey: "NX-b83a34b8",
+                SocketManager.tokenKey: Environment.getClientToken(config: config),
                 SocketManager.userIDKey: storage.userID,
                 SocketManager.sdkVersionKey: userpilot?.version() ?? "",
                 SocketManager.autoPropertiesKey: autoProperties,
