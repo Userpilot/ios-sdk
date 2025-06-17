fc1c679d05db42c7e8f8f4c0c9deeb29c9c9ab1c
motasem-userpilot <motasem@userpilot.co>
2025-06-17 12:24:56 +0300

    ⬆️ Bump SDK version

--- Sources/Userpilot/Version.swift
+++ Sources/Userpilot/Version.swift
@@ -14,5 +14,5 @@ import Foundation
 /// The current version of the Userpilot SDK.
 private let versionMajor = "1"
 private let versionMinor = "0"
-private let versionPatch = "0"
+private let versionPatch = "1"
 let userpilotVersion = "\(versionMajor).\(versionMinor).\(versionPatch)"
