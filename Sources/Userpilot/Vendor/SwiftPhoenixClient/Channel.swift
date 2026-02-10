8a6a8795b5368d0b67f9dc6e9bb3038c8ae8bc78
Motasem <motasem@userpilot.com>
2026-02-10 12:04:01 +0200

    Release 1.0.16
    🐛 Fix threading issue on PhoenixTransportDelegate Socket layer.
    
    ⬆️ Bump SDK version.

--- Sources/Userpilot/Vendor/SwiftPhoenixClient/Channel.swift
+++ Sources/Userpilot/Vendor/SwiftPhoenixClient/Channel.swift
@@ -98,6 +98,9 @@ public class Channel {
   /// Refs of stateChange hooks
   var stateChangeRefs: [String]
   
+  /// Serial queue from the parent socket for thread-safe state access
+  let socketQueue: DispatchQueue
+  
   /// Initialize a Channel
   ///
   /// - parameter topic: Topic of the Channel
@@ -114,7 +117,9 @@ public class Channel {
     self.joinedOnce = false
     self.pushBuffer = []
     self.stateChangeRefs = []
+    self.socketQueue = socket.queue
     self.rejoinTimer = TimeoutTimer()
+    self.rejoinTimer.queue = TimerQueue(dispatchQueue: socket.queue)
     
     // Setup Timer delgation
     self.rejoinTimer.callback
